e06ebf196f8e279d7bd3f7106364756e9d74ab4b
P-Kempf <paulonwein@gmail.com>
2022-05-12 21:21:46 +0200

    bikram los primeros ejercicios

---
+++ ejercicios/JS/bikram_0/paul.js
@@ -0,0 +1,24 @@
+let variableSinValor = [];
+console.log(1,variableSinValor);
+
+let booleano1 = true;
+let booleano2 = false;
+console.log(2,booleano1, booleano2);
+
+const PI = 3.14;
+console.log(3, PI);
+
+const TAU = PI*2;
+console.log(4, TAU);
+
+var booleanoAnd = booleano1 == booleano2;
+console.log(5, booleanoAnd);
+
+var booleanoNot = booleano1 != booleano2;
+console.log(6, booleanoNot);
+
+var booleanoMix0 = booleano1 || booleano2;
+console.log(booleanoMix0)
+
+
+
