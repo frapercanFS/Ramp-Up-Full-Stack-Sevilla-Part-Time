d2d15046ade07626b4b45ec3bd4b3681a0db3122
Ulyssbat <stewartbatallasminda@gmail.com>
2022-05-14 01:02:51 +0200

    inicio del ejercicio de variables

---
+++ ejercicios/JS/stewart.js
@@ -0,0 +1,18 @@
+let variableSinValor = undefined;
+console.log(1, variableSinValor);
+
+let booleano1 = Boolean(true);
+let booleano2 = Boolean(false);
+console.log(2, booleano1, booleano2);
+
+const pi = 3.14;
+console.log(3, pi);
+
+const tau = pi*2;
+console.log(4, tau);
+
+var booleanoAnd = booleano1 == booleano2;
+console.log(5, booleanoAnd);
+
+var incrementarDesp = 2;
+var resultadoDesp = 
