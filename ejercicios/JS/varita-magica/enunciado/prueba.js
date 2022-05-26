aa522584c28d9a53219cfc69a28a869126ae104f
xaxi <francisco.perez@thebridgeschool.es>
2022-05-26 20:23:50 +0200

    varita

---
+++ ejercicios/JS/varita-magica/enunciado/prueba.js
@@ -0,0 +1,8 @@
+const colors = ['red', 'blue', 'green','purple','yellow']
+const numeros = [1,2,3,4,5]
+const arrayBidimensional = [[1,2,3],[5,3,2],[53903,2]]
+
+
+
+
+console.log(getRandom(arrayBidimensional))
