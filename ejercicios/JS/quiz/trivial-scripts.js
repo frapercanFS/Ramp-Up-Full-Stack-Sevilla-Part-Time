7cbfeef8d59e37fdc9f59fe3bbc1a127dc0a1ac5
P-Kempf <paulonwein@gmail.com>
2022-05-31 16:19:03 +0200

    version del trivial con botones operativos con tan solo 1 pregunta

---
+++ ejercicios/JS/quiz/trivial-scripts.js
@@ -0,0 +1,93 @@
+const preguntas = [{
+    pregunta: "En el mundo de Mazmorras y Dragones ¿Quién es Elminster?",
+    opciones: ["Un Bardo", "Un Mercader", "Un Mago", "Un Marinero"],
+    respuesta: "Un Mago"
+},
+/* {
+    pregunta: "¿Quién de estos personajes no es un Hobitt?",
+    opciones: ["Frodo", "Sam", "Golum", "Bardo"],
+    respuesta: "Bardo"
+},
+{
+    pregunta: "¿Cual es el nombre de la ciudad bajo la montaña del Hobbit?",
+    opciones: ["Minas tirith", "Erebor", "Rivendell", "Abismo de Helm"],
+    respuesta: "Erebor"
+} */]
+class Pregunta {
+    constructor(pregunta, opciones, respuesta) {
+        this.pregunta = pregunta;
+        this.opciones = opciones;
+        this.respuesta = respuesta;
+    }
+
+    comprobarOpcion(eleccion) {
+        return eleccion === this.respuesta
+    }
+}
+
+let listaPreguntas = [];
+for (let pregunta of preguntas) {
+    listaPreguntas.push(new Pregunta(pregunta.pregunta, pregunta.opciones, pregunta.respuesta));
+
+}
+let htmlNuevo = "";
+for (let pregunta of listaPreguntas) {
+    htmlNuevo += imprimePregunta(pregunta);
+}
+
+function imprimePregunta(pregunta) {
+    let newHTML = "";
+
+    newHTML += imprimeTitulo(pregunta);
+    newHTML += imprimeTodasLasRespuestas(pregunta);
+
+    return newHTML;
+}
+
+function imprimeTitulo(pregunta) {
+    let tituloPregunta = pregunta.pregunta;
+    document.getElementById("pregunta").innerText = tituloPregunta;
+}
+
+function imprimeTodasLasRespuestas(pregunta) {
+
+    for (let i = 0; i < pregunta.opciones.length; i++) {
+        const opcion = document.createElement("button");
+        opcion.innerText = pregunta.opciones[i];
+        opcion.className = 'button';
+        document.getElementById("opciones").appendChild(opcion);
+        opcion.addEventListener("click", opcionSeleccionada)
+
+    }
+    function opcionSeleccionada(e) {
+        let opcionElegida = e.target.innerText;
+        let score = 0;
+        if (pregunta.comprobarOpcion(opcionElegida)) {
+            score++
+        }
+        console.log(score)
+        console.log(pregunta.comprobarOpcion(opcionElegida))
+
+        console.log(opcionElegida)
+    }
+}
+
+
+/* function comprobarOpcion(eleccion) {
+    let solucion = Pregunta.respuesta;
+    console.log(solucion)
+    if (eleccion == solucion) {
+        console.log(1);
+    }
+    else {
+        console.log(0);
+    }
+} */
+
+/* }
+function opcionSeleccionada(e) {
+    
+
+} */
+
+
