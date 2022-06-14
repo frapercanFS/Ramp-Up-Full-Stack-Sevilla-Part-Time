603ab2c246c4c2cf62db68e29f84fdb388804c20
xaxi <francisco.perez@thebridgeschool.es>
2022-06-14 21:24:29 +0200

    corregida pull request #2

--- ejercicios/JS/teoria_5/canales.js
+++
@@ -1,6 +0,0 @@
-function mostrarTodasRazas(){
-    fetch('https://dog.ceo/api/breeds/list/all')
-    .then(res => res.text())
-    .then(text => document.getElementById('razas').innerHTML = text);
-}
-mostrarTodasRazas()
