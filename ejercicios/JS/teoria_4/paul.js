9fb15831ff11fead79228e9fe8760fc0b202e150
P-Kempf <paulonwein@gmail.com>
2022-06-14 18:38:50 +0200

    ejercicio de local-storage resuelto con lista de usuarios registrados y borrados

--- ejercicios/JS/teoria_4/paul.js
+++ ejercicios/JS/teoria_4/paul.js
@@ -22,10 +22,17 @@ function guardarDatos(e) {
         localStorage.setItem("usuarios", JSON.stringify([]))
     }
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    console.log(usuarios)
     usuarios.push(user)
+
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
     datosFormulario = localStorage.getItem("usuarios");
+
+    const usuariosRegistrados = document.createElement("ul");
+
+    document.getElementById("usuarios-registrados").appendChild(usuariosRegistrados);
+
+    usuariosRegistrados.innerHTML = `<li id="usuario-registrado"><b>Nombre:</b>${user.nombre}   <b>Email:</b>${user.email}<li>`
 
 
 
@@ -37,12 +44,37 @@ let botonClear = document.getElementById("boton-borrar")
 botonClear.addEventListener("click", borrarDatosFormulario)
 
 function borrarDatosFormulario() {
-
-    console.log(JSON.parse(localStorage.getItem("usuarios")).length);
+    let listadoDeUsuarios = JSON.parse(localStorage.getItem("usuarios"))
     for (let i = 0; i < JSON.parse(localStorage.getItem("usuarios")).length; i++) {
-        console.log("Un usuario")
+        const botonesParaBorrar = document.createElement("div");
+
+
+        botonesParaBorrar.innerHTML = `<button id="botones-borrado" onclick="eliminarUsuario(${i})" class="button">
+                                       ${listadoDeUsuarios[i].nombre}</button>`;
+        document.getElementById("opciones-borrado").appendChild(botonesParaBorrar);
+
     }
-
-
+    console.log(JSON.parse(localStorage.getItem("usuarios")))
 }
+function eliminarUsuario(i) {
+    let listadoDeUsuarios = JSON.parse(localStorage.getItem("usuarios"))
+    let listadoDeUsuariosBorrados = listadoDeUsuarios.splice(i, 1);
+    localStorage.setItem("usuarios", JSON.stringify(listadoDeUsuarios));
+    console.log(listadoDeUsuariosBorrados[0].nombre)
+    const usuariosBorrados = document.createElement("ul");
+
+
+    usuariosBorrados.innerHTML = `<li id="usuario-eliminado">
+    <b>Nombre:</b>${listadoDeUsuariosBorrados[0].nombre} 
+      <b>Email:</b>${listadoDeUsuariosBorrados[0].email}<li>`
+    document.getElementById("usuarios-eliminados").appendChild(usuariosBorrados);
+
+    var antiguosBotones = document.querySelectorAll("#botones-borrado")
+    for(let i=0; i<antiguosBotones.length;i++){
+        antiguosBotones[i].style.display = "none";
+    }
+    borrarDatosFormulario();
+}
+
+localStorage.clear()
 
