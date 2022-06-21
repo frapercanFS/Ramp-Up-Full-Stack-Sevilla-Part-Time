57ab2a99e52b4bacde1c3a408e54f9407ee8991d
xaxi <francisco.perez@thebridgeschool.es>
2022-06-21 21:29:59 +0200

    ejercicio terminado

--- ejercicios/JS/teoria_5/canales.js
+++ ejercicios/JS/teoria_5/canales.js
@@ -63,3 +63,44 @@ function crearOpcionesRazasHTML(listaPerros) {
     document.getElementById("listaRazas").appendChild(elemento);
   }
 }
+
+function mostrarUsuarioGit(usuario) {
+  fetch(`https://api.github.com/users/${usuario}`)
+    .then((response) => response.json())
+    .then((json) => crearUsuarioHTML(json))
+    .catch((error) => alert(error));
+}
+
+function crearUsuarioHTML(json) {
+  let nombre = json.name;
+  let numRepos = json.public_repos;
+  let avatar = json.avatar_url;
+
+  nombreHTML = document.createElement("p");
+  nombreHTML.innerHTML = nombre;
+  document.getElementById("nombreUsuario").appendChild(nombreHTML);
+
+  numReposHTML = document.createElement("p");
+  numReposHTML.innerHTML = numRepos;
+  document.getElementById("numeroRepos").appendChild(numReposHTML);
+
+  avatarHTML = document.createElement("img");
+  avatarHTML.src = avatar;
+  document.getElementById("avatar").appendChild(avatarHTML);
+}
+
+let names = ["iliakan", "remy", "jeresig"];
+
+let requests = names.map((name) =>
+  fetch(`https://api.github.com/users/${name}`)
+);
+
+Promise.all(requests)
+  .then((responses) => {
+    // todas las respuestas son resueltas satisfactoriamente
+    return responses;
+  })
+  // mapea el array de resultados dentro de un array de response.json() para leer sus contenidos
+  .then((responses) => Promise.all(responses.map((r) => r.json())))
+  // todas las respuestas JSON son analizadas: "users" es el array de ellas
+  .then((users) => users.forEach((user) => console.log(user.name)));
