1d4b2a59619fd03f7b901b5b86da9e052dc41452
xaxi <francisco.perez@thebridgeschool.es>
2022-09-30 20:43:19 +0200

    ejercicio mysql

--- ejercicios/JS/web_scrapping/canales.js
+++ ejercicios/JS/web_scrapping/canales.js
@@ -1,18 +1,38 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+var lodash = require("lodash");
 
-const axios = require('axios');
-const cheerio = require('cheerio');
+const csv = require("fast-csv");
+const fs = require("fs");
 
+
+const csvFile = fs.createWriteStream('prueba.csv');
+
+const csvStream = csv.format({ headers: true });
 
 
 async function extraerPagina(pagina) {
-    const pageContent = await axios.get(`https://www.meneame.net/${pagina}`);
-    const $ = cheerio.load(pageContent.data);
+  const pageContent = await axios.get(`https://www.meneame.net/${pagina}`);
+  const $ = cheerio.load(pageContent.data);
 
-    const titulos = $('h2').map((_, el) => {
-        el = $(el);
-        console.log(el.text())
-        console.log('-------------------')
-    }).get();
+  const titulos = $("h2")
+    .map((i, el) => {
+      el = $(el);
+      csvStream.write({ titulo: el.text(), header2: "row1-col2" });
+      return el.text();
+    })
+    .get();
 }
 
-extraerPagina('')
+// extraerPagina(0)
+
+paginas = lodash.range(1, 50);
+
+for (pagina of paginas) {
+  extraerPagina(pagina);
+}
+
+csvStream.pipe(csvFile).on("end", (file) => {
+    process.exit();
+  });
+  
